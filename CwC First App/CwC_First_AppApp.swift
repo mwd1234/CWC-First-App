af3336ff267a1de522375b4ce2fc23593435efcc
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-23 17:35:49 -0700

    Initial Commit

---
+++ CwC First App/CwC_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CwC_First_AppApp.swift
+//  CwC First App
+//
+//  Created by Matthew Dembinski on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CwC_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
